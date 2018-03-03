c651549a41ce115f7fb329640a339590be27bf2e
Adam McAmis <amcamis@jellyvision.com>
2018-03-03 16:09:35 -0600

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'containers/App';
-
-import 'styles/index.scss';
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-
-export default combineReducers({
-  router: routerReducer,
-});
-
-export const routerSelector = state => state.router;
