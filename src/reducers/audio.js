be8982672d1aa482d829be42c8908f24454ef2ff
Adam McAmis <mcamis@gmail.com>
2019-08-13 22:46:28 -0400

    Add some Rodney

--- src/reducers/audio.js
+++ src/reducers/audio.js
@@ -8,6 +8,7 @@ import OutHere from '../songs/06_Out_Here_At_The_Outersphere.mp3';
 import Chicago from '../songs/07_Chicago.mp3';
 import Passports from '../songs/08_Passports.mp3';
 import ComeTrue from '../songs/09_Come_True.mp3';
+import Shuffle from '../songs/shuffle.mp3';
 
 export const defaultState = {
   currentTrack: null,
@@ -17,79 +18,15 @@ export const defaultState = {
   playing: false,
   paused: false,
   playlist: [
-    'Chaos',
-    'Akihabara',
-    'Nothing Stops Detroit',
-    'Gnosis Hardware',
-    'No Refuge',
-    'Out Here at the Outersphere',
-    'Chicago',
-    'Passports',
-    'Come True',
+    'Shuffle',
   ],
   tracks: {
-    ['Chaos']: {
-      file: Chaos,
+    ['Shuffle']: {
+      file: Shuffle,
       track: 1,
-      album: 'Entertainment System',
-      artist: 'Professor Kliq',
-      title: 'Chaos',
-    },
-    ['Akihabara']: {
-      title: 'Akihabara',
-      file: Akihabara,
-      track: 2,
-      album: 'Entertainment System',
-      artist: 'Professor Kliq',
-    },
-    ['Nothing Stops Detroit']: {
-      title: 'Nothing Stops Detroit',
-      file: NothingStopsDetroit,
-      track: 3,
-      album: 'Entertainment System',
-      artist: 'Professor Kliq',
-    },
-    ['Gnosis Hardware']: {
-      title: 'Gnosis Hardware',
-      file: GnosisHardware,
-      track: 4,
-      album: 'Entertainment System',
-      artist: 'Professor Kliq',
-    },
-    ['No Refuge']: {
-      title: 'No Refuge',
-      file: NoRefuge,
-      track: 5,
-      album: 'Entertainment System',
-      artist: 'Professor Kliq',
-    },
-    ['Out Here at the Outersphere']: {
-      title: 'Out Here at the Outersphere',
-      file: OutHere,
-      track: 6,
-      album: 'Entertainment System',
-      artist: 'Professor Kliq',
-    },
-    ['Chicago']: {
-      title: 'Chicago',
-      file: Chicago,
-      track: 7,
-      album: 'Entertainment System',
-      artist: 'Professor Kliq',
-    },
-    ['Passports']: {
-      title: 'Passports',
-      file: Passports,
-      track: 8,
-      album: 'Entertainment System',
-      artist: 'Professor Kliq',
-    },
-    ['Come True']: {
-      title: 'Come True',
-      file: ComeTrue,
-      track: 9,
-      album: 'Entertainment System',
-      artist: 'Professor Kliq',
+      album: 'Diamond Inside of You',
+      artist: 'Rodney Franklin',
+      title: 'Malibu Shuffle',
     },
   },
 };
