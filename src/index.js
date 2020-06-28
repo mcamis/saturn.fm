e530e2c6ee40b7a865090a0d66d96924375c012d
mcamis <mcamis@gmail.com>
2020-06-27 21:32:36 -0400

    Preload image assets during splash screen

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import { Provider } from "react-redux";
 import configureStore from "store/configure";
 import "styles/index.scss";
 
+import dashboard from "images/dashboard.png";
+import header from "images/header.png";
+import globeSprite from "images/globeSprite.png";
+import discSrc from "images/disc.png";
+import moreSrc from "images/more.png";
+import hideSrc from "images/hide.png";
+import rwdSrc from "images/rwd.png";
+import playSrc from "images/play-pause.png";
+import ffwdSrc from "images/ffwd.png";
+import stopSrc from "images/stop.png";
+import repeatSrc from "images/repeat.png";
+import textureSrc from "images/texture.gif";
+import pinkSrc from "images/pink.gif";
+import orbShadow from "images/orb-shadow.png";
 import * as Sentry from "@sentry/browser";
 
 if (process.env.NODE_ENV === "production") {
@@ -29,9 +43,34 @@ window.addEventListener("orientationchange", () => {
   setManualViewportHeight();
 });
 
+function preloadImages() {
+  const imageSrcs = [
+    dashboard,
+    header,
+    globeSprite,
+    discSrc,
+    moreSrc,
+    hideSrc,
+    rwdSrc,
+    playSrc,
+    ffwdSrc,
+    stopSrc,
+    repeatSrc,
+    textureSrc,
+    pinkSrc,
+    orbShadow,
+  ];
+
+  imageSrcs.forEach((srcUrl) => {
+    const img = new Image();
+    img.src = srcUrl;
+  });
+}
+
 // Mobile Safari doesn't provide a correct `window.innerHeight` on initial render
 setTimeout(() => {
   setManualViewportHeight();
+  preloadImages();
 
   ReactDOM.render(
     <Provider store={store}>
