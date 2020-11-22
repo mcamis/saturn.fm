efab06a45253f63ef1113c38c41db0f77cd51012
Adam McAmis <mcamis@gmail.com>
2020-11-22 13:19:16 -0500

    WIP basic storybook implementation

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.story.js"],
+  addons: [
+    "@storybook/preset-scss",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+  ],
+};
---
+++ src/components/FileReader.story.js
@@ -0,0 +1,34 @@
+/* eslint-disable react/jsx-props-no-spreading */
+// Button.stories.js
+
+import React from "react";
+import FileReader from "./FileReader";
+
+import { defaultState } from "../reducers/audio";
+import "../styles/index.scss";
+
+const props = {
+  audio: {
+    tracks: defaultState.tracks,
+    playlist: defaultState.playlist,
+  },
+  arrangeTracks: () => {},
+  addTracks: () => {},
+  removeTrack: () => {},
+  toggleMenu: () => {},
+};
+
+const Template = (args) => <FileReader {...args} />;
+
+export const FirstStory = Template.bind({});
+
+FirstStory.args = {
+  ...props,
+  /* the args you need here will depend on your component */
+};
+
+// This default export determines where your story goes in the story list
+export default {
+  title: "FileReader",
+  component: FileReader,
+};
