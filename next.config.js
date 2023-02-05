c88194d7543f47b527149f52cdb3a5f8e5de111f
Adam McAmis <mcamis@gmail.com>
2023-02-04 23:01:09 -0500

    Restore mp3 loading

--- next.config.js
+++ next.config.js
@@ -1,15 +1,26 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  webpack: (config) => {
+  webpack(config, options) {
+    const { isServer } = options;
     config.module.rules.push({
-      test: /\.mp3$/,
-      use: {
-        loader: "file-loader",
-      },
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      exclude: config.exclude,
+      use: [
+        {
+          loader: require.resolve('url-loader'),
+          options: {
+            limit: config.inlineImageLimit,
+            fallback: require.resolve('file-loader'),
+            publicPath: `${config.assetPrefix}/_next/static/`,
+            outputPath: `${isServer ? '../' : ''}static/`,
+            name: '[name]-[hash].[ext]',
+            esModule: config.esModule || false,
+          },
+        },
+      ],
     });
 
     return config;
   },
   reactStrictMode: false,
-  styledComponents: false,
 };
